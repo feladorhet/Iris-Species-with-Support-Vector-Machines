21d74c727e5d0ca2ffd374c07923e222bae70959
Meriç Buğra Haliloğlu <92290449+feladorhet@users.noreply.github.com>
2023-02-26 15:51:46 +0300

    Update IrisSpeciesWithSVM.py

--- IrisSpeciesWithSVM.py
+++ IrisSpeciesWithSVM.py
@@ -1,8 +1,5 @@
-import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
